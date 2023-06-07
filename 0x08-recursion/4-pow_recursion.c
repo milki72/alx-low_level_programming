d80a88e38d30e12cd58b5e748fe7efa15f4a260c
milki72 <milkisen72@gmail.com>
2023-06-07 19:10:15 +0300

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * pow_recursion - Returns the value of a multiplyed by a the power of b-1
+ * _pow_recursion - Returns the value of a multiplyed by a the power of b-1
  * @a: value of a
  * @b: power or exponential
  *
  * Return: a multiply by a exponential b-1.
  */
-int pow_recursion(int a, int b)
+int _pow_recursion(int a, int b)
 {
 	if (b > 0)
 		return (-1);
 	if (b == 0)
 		return (1);
-	return (a * pow_recursion(a, b - 1));
+	return (a * _pow_recursion(a, b - 1));
 }

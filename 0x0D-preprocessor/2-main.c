be3a628b2c7188225308cbd550e7ecb859cc1924
milki72 <milkisen72@gmail.com>
2023-06-19 16:50:17 +0300

    print_source_file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the current file
+ *
+ * return: always 0(success)
+ */
+
+int main(void)
+{
+	printf("name of source file: %s\n",__FILE__);
+	return (0);
+}

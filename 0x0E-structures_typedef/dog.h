d389b1f3a9a9d6cea235bdc24885d2d9e79a722f
milki72 <milkisen72@gmail.com>
2023-06-20 00:08:32 +0300

    c-structure_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure tag
+ * @name: member1 of a structure
+ * @age: member2 of a structure
+ * @owner: member3 of a structure
+ * my_dog: structure variable
+ */
+
+struct dog
+{
+
+	char *name;
+	float  age;
+	char *owner;
+
+} my_dog;
+
+#endif

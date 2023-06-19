53395551cb6b1dbc78f50342df05a72d66a634ec
milki72 <milkisen72@gmail.com>
2023-06-19 16:17:52 +0300

    c-preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,10 @@
-#include "main.h"
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
 /**
  * SIZE - it is a defined macro name
  * 1024:it is an expansion for a given macro
  */
 #define SIZE 1024
+
+#endif
--- 0x0D-preprocessor/main.h
+++
@@ -1,4 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#define SIZE_H
-#endif

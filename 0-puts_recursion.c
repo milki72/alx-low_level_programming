7ee474395f03e86682b3cf957fc4040101167391
milki72 <milkisen72@gmail.com>
2023-08-05 09:36:16 +0300

    c-recursion

---
+++ 0-puts_recursion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - this function prints a string followed by a new line.
+ * @s: it is the string parameter that is passes to the function to be printed.
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+        if (*s == '0')
+        {
+                _putchar('\n');
+        }
+        else if (*s)
+        {
+                _putchar(*s);
+                _puts_recursion(s + 1);
+        }
+        else
+        {
+                _putchar('\n');
+	}
+
+}
+

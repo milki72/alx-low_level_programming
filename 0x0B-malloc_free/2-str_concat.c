4783e495aa3029693c76dc549ea787795321f0bc
milki72 <milkisen72@gmail.com>
2023-08-08 00:06:58 +0300

    c-malloc_free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - concatenate two string parameter.
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: a pointer to string.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	if (s1 == NULL && s2 == NULL)
+	return (NULL);
+
+	size_t len1 = (s1 != (NULL)) ? strlen(s1) : 0;
+	size_t len2 = (s2 != (NULL)) ? strlen(s2) : 0;
+
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	size_t i, j;
+
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (j = 0; j < len2; j++)
+	{
+		ptr[i + j] = s2[j];
+	}
+
+	ptr[i + j] = '\0';
+
+	return (ptr);
+}
+

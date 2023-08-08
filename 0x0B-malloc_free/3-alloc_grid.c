a4f2fa2bc7258c6fd0130f71caeb93eb7726eea9
milki72 <milkisen72@gmail.com>
2023-08-08 19:37:39 +0300

    c-malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,21 +7,18 @@
  * @height: the size of height parameter.
  *
  * Return: a pointer to a pointer to integer.
- */ 
+ */
 
 int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int i;
 
-        // allocate memory for rows
 	grid = (int **)malloc(sizeof(int **) * height);
 
-        // memory allocation failed
 	if (grid == NULL)
 		return (NULL);
 
-	//allocate memory for each row
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(sizeof(int *) * width);
